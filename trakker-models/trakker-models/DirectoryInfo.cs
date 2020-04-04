8b37ce177bb1907f00728ccc84a5a7d5ea0ed247
Shaked Shuster <shaked6600@gmail.com>
2020-04-05 02:39:19 +0300

    Add CanAccess property

--- trakker-models/trakker-models/DirectoryInfo.cs
+++ trakker-models/trakker-models/DirectoryInfo.cs
@@ -8,26 +8,31 @@ namespace TrakkerModels
     public class DirectoryInfo : FileSystemNode
     {
         public List<FileSystemNode> Children { get; set; }
+        public bool CanAccess;
 
-        public DirectoryInfo(ulong size, string fullPath) : base(size, fullPath)
+        public DirectoryInfo(ulong size, string fullPath, bool canAccess=true) : base(size, fullPath)
         {
+            CanAccess = canAccess;
             this.Children = new List<FileSystemNode>();
         }
 
-        public DirectoryInfo(string fullPath) : base(fullPath)
+        public DirectoryInfo(string fullPath, bool canAccess=true) : base(fullPath)
         {
+            CanAccess = canAccess;
             this.FullPath = fullPath;
             this.Children = new List<FileSystemNode>();
         }
 
-        public DirectoryInfo(string fullPath, List<FileSystemNode> children) : base(fullPath)
+        public DirectoryInfo(string fullPath, List<FileSystemNode> children, bool canAccess=true) : base(fullPath)
         {
             this.Children = children;
+            CanAccess = canAccess;
         }
 
-        public DirectoryInfo(string fullPath, List<FileSystemNode> children,ulong size) : base(size,fullPath)
+        public DirectoryInfo(string fullPath, List<FileSystemNode> children,ulong size, bool canAccess=true) : base(size,fullPath)
         {
             this.Children = children;
+            CanAccess = canAccess;
         }
     }
 }
